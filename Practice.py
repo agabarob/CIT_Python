50042c36477a0ec12c3b6df85375e67077ac2e24
agabarob <agabarob@gmail.com>
2022-08-22 08:56:00 +0300

    My first program

---
+++ Practice.py
@@ -0,0 +1,10 @@
+print("Simpple Calculator ")
+print("Description: Returns the product of two integers if its greater than 1000 or the sum of the integers otherwise")
+a= int(input("Enter the first number: "))
+b= int(input("Enter the second number: "))
+d= a*b
+e= a+b
+if d>=1000:
+   print(d)
+else:
+   print(e)
